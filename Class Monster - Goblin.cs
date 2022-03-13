c1b288a9912f4d815ef0e5b35d1cf9204686ea85
ViniPetra <viniciuspetratti@hotmail.com>
2022-03-13 01:10:25 -0300

    All Classes created

--- Class Monster - Goblin.cs
+++ Class Monster - Goblin.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace DD
 {
-    internal class Class_Monster___Goblin
+    internal class Goblin : Monster
     {
+        //Empty constructor
+        public Goblin()
+        {
+
+        }
+
+        //Constructor
+        public Goblin(int level, Monster_Status status) : base(level, status)
+        {
+
+        }
+
+        public int Defense()
+        {
+            Random random = new Random(); //Instanciates random num
+            int randomNum = random.Next(1, this.Status.Luck); //Generates radom num based on Luck
+            double randDiv = (this.Status.Protection + this.Status.Agility) * (1 + randomNum / 100);
+            int defenseVal = ((int)randDiv); // Gets defense value
+            return defenseVal;
+        }
+
+        public int ReceiveAttack(int attackStrengh)
+        {
+            int defense = Defense();
+            int lifeReduction = defense - attackStrengh;
+            return lifeReduction;
+        }
     }
 }
